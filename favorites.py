11b5a67a901cae60ff78e9075c9ecb46b7835c38
iannolon <35769152+iannolon@users.noreply.github.com>
2018-02-14 08:35:05 -0500

    Create favorites.py

---
+++ favorites.py
@@ -0,0 +1,9 @@
+#IanNolon
+#1/14/18 - Valentine's day!
+#favorites.py
+
+word = input('Enter your favorite word: ')
+num = int(input('Enter your favorite number: '))
+i = 0
+for i in range (1,num+1):
+    print(word)
