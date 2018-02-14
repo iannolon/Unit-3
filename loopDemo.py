5f01646a7324941a55688aded55c6bc59ac21461
iannolon <35769152+iannolon@users.noreply.github.com>
2018-02-14 08:30:54 -0500

    Update loopDemo.py

--- loopDemo.py
+++ loopDemo.py
@@ -1,1 +1,60 @@
+#IanNolon
+#2/14/18 -- Valentine's Day!
+#loopDemo.py - how to use for and while loops
 
+'''
+#I love computer science 5 times
+for i in range(1,6):
+    print('I <3 Computer Science')
+'''
+
+'''
+#while loop version
+i = 0
+while i<5:
+    print('I <3 Computer Science')
+    i = i+1 #update step
+'''
+
+'''
+#Counts from 1 to 10
+for i in range(1,11):
+    print(i)
+'''
+
+'''
+i = 1
+while i<=10:
+    print(i)
+    i +=1 #means add 1 to i
+'''
+
+'''
+#Count from 27 to 43 by 2s
+for i in range(27,44,2): #Start, stop +1, and the step (how much you're going up by each time)
+    print(i)
+'''
+
+'''
+i=27
+while i>= 27 and i<=43:
+    print(i)
+    i+=2
+'''
+
+'''
+#add up the numbers from 1 to 5
+total = 0
+for i in range(1,6):
+    total = total + i 
+print(total)
+'''
+
+'''
+total = 0
+i=1
+while i<=5:
+    total +=i
+    i+= 1
+print(total)
+'''
